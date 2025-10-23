d7447a4d2694d700ebf895a40e46ea6e4671619e
caio14333 <caiosantiago14022010@gmail.com>
2025-10-23 11:58:45 -0300

    dsklsdj

---
+++ questoes.js
@@ -0,0 +1,8 @@
+let numero1 = prompt("digite o primeiro numero")
+let numero2 = prompt("digite o segundo numero")
+let numero3 = prompt("digite o segundo  numero")
+
+let soma = (numero1 + numero2)
+if (soma > numero3){
+    console.log(`A soma do numero ${numero1} + o numero ${numero2} é maior do que o numero ${numero3}`)
+}
